0bd9fa3f6f1773a29adce0049545668d8dfc8799
Whiskey <Berkoic@hotmail.com>
2024-02-26 20:02:49 +0300

    Update unit test
    - update Router.t.sol file

--- test/unit/Router.t.sol
+++ test/unit/Router.t.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.21;
 
 import { Test } from "forge-std/Test.sol";
-import "forge-std/console.sol";
 
 import { WETH } from "vectorized/solady/tokens/WETH.sol";
 import "openzeppelin-contracts/contracts/utils/math/Math.sol";
