f828d36042a30a2fcc797b0bb9773c797124c742
Can Vardar <ismailcanvardar@gmail.com>
2024-03-10 18:22:39 +0000

    chore: update deploy bash scripts

--- bin/deploy_script_local.sh
+++ bin/deploy_script_local.sh
@@ -16,5 +16,5 @@ forge script $script \
     --fork-url http://localhost:8545 \
     --broadcast \
     -vvvv \
-    --sender 0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 \
-    --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+    --sender $SENDER \
+    --private-key $PRIVATE_KEY
